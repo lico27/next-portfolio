fcff0c75ab79135e0c88c4c0f40e4c54d996736a
Liam Cottrell <liam@liamco.io>
2024-08-21 13:15:47 +0100

    initialise project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Liam Cottrell // Aspiring Data Scientist",
+  description: "Portfolio created by Liam to showcase my skills and projects",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
