c9f6679cf638889cc7db43bf224ea9bc1839e402
Liam Cottrell <liam@liamco.io>
2024-08-26 22:04:57 +0100

    edit navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          {name: 'Home', link: '/'}
-        ]}/>
+        <FloatingNav navItems={navItems}/>
         <Hero />
         <Grid />
       </div>
