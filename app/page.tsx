db8c81a909d0a851563dea9ebdc553fc893d9b92
Liam Cottrell <liam@liamco.io>
2024-08-28 15:12:22 +0100

    remove navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
       <div className="w-full">
-        <FloatingNav navItems={navItems}/>
         <Hero />
       </div>
     </main>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,12 +8,10 @@ const Hero = () => {
     <div className='pb-20 pt-36'>
         <div>
             <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='#3D7867' />
-            {/* <Spotlight className='top-10 -right-200 h-[80vh] w-[50vw]' fill='#649672' /> */}
             <Spotlight className='top-28 left-80 h-[80vh]' fill='#5AD767' />
         </div>
         
         <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-dot-white/[0.07] bg-dot-black/[0.2] flex items-center justify-center absolute top-0 left-0">
-            {/* Radial gradient for the container to give a faded look */}
             <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
 
