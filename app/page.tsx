7c9f2328143a2cd2392c19d5d3d0a0b52e1a3b3b
Liam Cottrell <liam@liamco.io>
2024-08-28 16:54:08 +0100

    fix scroll bug

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
-      <div className="w-full">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="w-full h-screen">
         <Hero />
       </div>
     </main>
