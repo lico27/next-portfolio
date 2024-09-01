8401d48e290ad2aa5b19cc3f939c686d5def6494
Liam Cottrell <liam@liamco.io>
2024-09-01 10:07:54 +0100

    change text

--- components/ScrollingSkills.tsx
+++ components/ScrollingSkills.tsx
@@ -5,8 +5,10 @@ import { skills } from '@/data/index';
 const ScrollingSkills = () => {
     return (
         <div>
-            <div>
-                <h2 className="relative z-10 heading !text-xl !md:text-2xl underline decoration-2 decoration-green-100 underline-offset-4">I have experience using:</h2>
+            <div className="flex w-full items-center justify-center">
+                <h2 className="relative z-10 heading !text-xl !md:text-2xl">I have&nbsp;</h2>
+                <h2 className="relative z-10 heading !text-xl !md:text-2xl  underline decoration-2 decoration-green-100 underline-offset-4">experience</h2>
+                <h2 className="relative z-10 heading !text-xl !md:text-2xl">&nbsp;using:</h2>
             </div>
             <div className="h-[10rem] mb-5 rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
               <InfiniteMovingCards
