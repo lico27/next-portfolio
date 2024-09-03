f6d3faad09269716b21d89c879dfa9da3035c32f
Liam Cottrell <liam@liamco.io>
2024-09-03 14:31:09 +0100

    change subheading

--- components/ScrollingSkills.tsx
+++ components/ScrollingSkills.tsx
@@ -6,9 +6,11 @@ const ScrollingSkills = () => {
     return (
         <div>
             <div className="flex w-full items-center justify-center">
-                <h2 className="relative z-10 heading !text-xl !md:text-2xl">I have&nbsp;</h2>
-                <h2 className="relative z-10 heading !text-xl !md:text-2xl  underline decoration-2 decoration-green-100 underline-offset-4">experience</h2>
-                <h2 className="relative z-10 heading !text-xl !md:text-2xl">&nbsp;using:</h2>
+                <h2 className="relative z-10 subheading">I have&nbsp;</h2>
+                <h2 className="relative z-10 subheading underline decoration-2 decoration-green-100 underline-offset-4">experience</h2>
+                <h2 className="relative z-10 subheading">&nbsp;with these&nbsp;</h2>
+                <h2 className="relative z-10 subheading underline decoration-2 decoration-green-100 underline-offset-4">technologies</h2>
+                <h2 className="relative z-10 subheading">:</h2>
             </div>
             <div className="h-[10rem] mb-5 rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
               <InfiniteMovingCards
